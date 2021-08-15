e907c4af3e6afdb1ed1362e3bdae39cfe3b2e2f5
scouri <eneblank19@protonmail.com>
2021-08-15 19:07:49 -0400

    shell commands to check if db exists

--- Sakuri.cs
+++ Sakuri.cs
@@ -1,15 +1,26 @@
 using System;
 using Npgsql;
+using System.Diagnostics;
 
 namespace Sakuri
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
-            var ConnectionString = "Host=localhost;Username=postgres;Password=s$cret;Database=Sakuri.db";
-            using var Connection = new NpgsqlConnection(ConnectionString);
-
+            var OutPut = "psql -lqt | cut -d \\| -f 1 | grep -qw SakuriDB".Bash();
+            var ConnectionString = "Host=localhost;Username=scouri;Password=PASSWORD;";
+            var Connection = new NpgsqlConnection(ConnectionString);
+            var CreateDatabase = new NpgsqlCommand("CREATE DATABASE SakuriDB WITH OWNER=scouri ENCODING = 'UTF8'", Connection);
+            Connection.Open();
+            CreateDatabase.ExecuteNonQuery();
+            Connection.Close();
+            
+            ConnectionString = "Server=localhost;User Id=scouri;Password=PASSWORD;Database=SakuriDB";
+            Connection = new NpgsqlConnection(ConnectionString);
+            CreateDatabase = new NpgsqlCommand("CREATE TABLE table1(ID CHAR(256) CONSTRAINT id PRIMARY KEY, Title CHAR)", Connection);
+            Connection.Open();
+            CreateDatabase.ExecuteNonQuery();
             var SQL = "SELECT version();";
 
             using var cmd = new NpgsqlCommand(SQL, Connection);
@@ -18,6 +29,30 @@ namespace Sakuri
 
             Console.WriteLine($"Postgres version: {Version}");
 
+            Connection.Close();
+        }
+        public static string Bash(this string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+            StartInfo = new ProcessStartInfo
+            {
+            FileName = "/bin/bash",
+            Arguments = "",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            }
+            };
+
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            return result;
         }
     }
+    
 }

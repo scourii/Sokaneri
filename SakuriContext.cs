5bcf57da74a2478fa9f23a75c838a7153afea62a
scouri <eneblank19@protonmail.com>
2021-08-30 20:23:13 -0400

    Create SakuriContext.cs

---
+++ SakuriContext.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sakuri
+{
+    public class SakuriContext : DbContext
+    {
+        public DbSet<MonthlyExpenses> MonthlyExpenses { get; set; }
+        public DbSet<YearlyExpenses> YearlyExpenses { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql("Blank");
+    }
+    public class MonthlyExpenses
+    {
+        public string ItemName { get; set; }
+        public int ItemPrice { get; set; }
+        public string Time { get; set; }
+        public string ItemCategory { get; set; }
+    }
+    public class YearlyExpenses
+    {
+        public string ItemName { get; set; }
+        public int ItemPrice { get; set; }
+        public string Time { get; set; }
+        public string ItemCategory { get; set; }
+    }
+}

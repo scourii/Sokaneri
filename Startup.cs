9fb33e0355a34dc96f5a326734676b5f09799a46
scouri <eneblank19@protonmail.com>
2021-09-06 21:29:41 -0400

    utilizing secrets.json for connection now

--- Startup.cs
+++ Startup.cs
@@ -31,14 +31,10 @@ namespace Sakuri
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<MoneyInformationService>();
-
-            var connectionString = Configuration["PostgreSql:ConnectionString"];
-            var dbPassword = Configuration["PostgreSql:DbPassword"];
-            var builder = new NpgsqlConnectionStringBuilder(connectionString)
+            services.AddDbContext<SakuriContext>(options =>
             {
-                Password = dbPassword
-            };
-            services.AddDbContext<SakuriContext>(options => options.UseNpgsql(Configuration.GetConnectionString("SakuriContext")));
+                options.UseNpgsql(Configuration.GetConnectionString("SakuriDatabase"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

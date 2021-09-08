da8b8715443ee817d989088ba85ff04a4091a8f6
scouri <eneblank19@protonmail.com>
2021-09-07 21:36:45 -0400

    Create AppRouteView.cs

---
+++ Herlpers/AppRouteView.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Net;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Sakuri.Helpers
+{
+    public class AppRouteView : RouteView
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public IAccountService AccountService { get; set; }
+        
+        protected override void Render(RenderTreeBuilder renderTreeBuilder)
+        {
+            var authorization = Attribute.GetCustomAttribute(RouteData.PageType, typeof(Authorization)) != null;
+            if (authorization && AccountService.User != null)
+            {
+                var returnUrl = WebUtility.UrlEncode(new Uri(NavigationManager.Uri).PathAndQuery);
+                NavigationManager.NavigateTo($"account/login?returnUrl={returnUrl}");
+            }
+            else
+            {
+                base.Render(renderTreeBuilder);
+            }
+        }
+    }
+}

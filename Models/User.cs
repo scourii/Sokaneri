0c2f4a61bbdd4de34eaa4269b4953035b3b50b22
scouri <eneblank19@protonmail.com>
2021-09-08 20:09:13 -0400

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+namespace Sakuri.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Token { get; set; }
+        public bool Delete { get; set; }
+    }
+}

57810cdbf6904e230ffc744cee31341cf54323ef
scouri <eneblank19@protonmail.com>
2022-03-12 20:31:17 +0000

    Update ApplicationUser.cs

--- Areas/Identity/Data/ApplicationUser.cs
+++ Areas/Identity/Data/ApplicationUser.cs
@@ -1,17 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
+using Sakuri.Models;
 
 namespace Sakuri.Areas.Identity.Data
 {
 
     public class ApplicationUser : IdentityUser
     {
-        
-        [Key]
-        [PersonalData]
-        [Column(TypeName = "bigint")]
-        public long userid { get; set; }
-        public virtual ICollection<Items> Items {get; set;}
+        public ICollection<Items> Items {get; set;}
     }
 }
